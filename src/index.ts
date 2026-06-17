50adebc3c61b9e9c6189013cde1e9e3b84950367
talimirchuk <talii.mirr@gmail.com>
2026-06-17 15:29:49 -0300

    Consigna base

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+  let resultado: string = "";
+  let contador: number = 0;
+
+  while (contador < cantidad) {
+    resultado = resultado + letra;
+    contador = contador + 1;
+  }
+
+  return resultado;
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
+
+let fila: number = 0;
+let contadorFilas: number=0;
+while (fila<tamaño) {
+  console.log(repetirLetra(caracter, tamaño));
+  fila = fila + 1;
+
+}
 
 // COMPLETAR
